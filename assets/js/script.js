9384f346b960ab1eab969f15daaeee59a250d10c
Keegam Jervis <viakeegan@gmail.com>
2022-01-05 15:06:30 -0500

    initial js ids

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// variables
+let myAPIWeatherKey = "&units=imperial&APPID=a15f02adbde7a78309af009d972b4360";
+let userSearch = "";
+let storeKey = localStorage.length;
+
+// all ids for the 5 day forecast
+let dayOneItems = [
+    "#dayOneDate",
+    "#dayOneIcon",
+    "#dayOneTemp",
+    "#dayOneWind",
+    "#dayOneHumid",
+    "#dayOneUV"
+];
+
