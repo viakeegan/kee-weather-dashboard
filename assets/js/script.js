9ebbeeb368aa744972e9e7c1dddc028fd8ed2379
Keegam Jervis <viakeegan@gmail.com>
2022-01-05 15:11:31 -0500

    pull lat and lon to make one call

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,4 +55,42 @@ function makingWeatherCall(weather) {
     .then(response => response.json())
     .then(function(data) {
 
-        
+        // Pull the lattitude and longitude and use that to make a onecall
+        // to openweathermap to get current and future forecasts.
+        let oneCallAPI = fetchOneCall(data.coord.lat, data.coord.lon);
+        fetch(oneCallAPI)
+        .then(response => response.json())
+        .then(function(data) {
+            // Display current temp
+            let currentTemp = Math.round(data.current.temp);
+            $("#curDayTemp").text(currentTemp + "*F");
+
+            // Display current wind speed
+            let curWind = Math.round(data.current.wind_gust);
+            $("#curDayWind").text(curWind + " MPH");
+
+            // Display current humidity
+            let curHumid = Math.round(data.current.humidity);
+            $("#curDayHumid").text(curHumid + "%");
+
+            // Display current UV index
+            let curUV = data.current.uvi;
+            // color coding based on the intensity of the UV Index
+            if (curUV < 3) {
+                $("#curDayUV").css("background-color", "rgb(0,150,0)");
+            }
+            else if (curUV >=3 && curUV < 6) {
+                $("#curDayUV").css("background-color", "rgb(196, 196, 23)");
+            }
+            else if (curUV >= 6 && curUV < 8) {
+                $("#curDayUV").css("background-color", "rgb(196, 92, 23)");
+            }
+            else if (curUV >= 8 && curUV < 11) {
+                $("#curDayUV").css("background-color", "rgb(175, 0, 0)");
+            }
+            else if (curUV >= 11) {
+                $("#curDayUV").css("background-color", "rgb(121, 0, 40)");
+            }
+            $("#curDayUV").text(curUV);
+
+            
